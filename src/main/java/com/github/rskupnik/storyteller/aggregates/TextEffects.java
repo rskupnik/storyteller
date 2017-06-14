b7c02b40f407bff40d287cbd45aa6af10eb96711
Myzreal <r.skupnik@gmail.com>
2017-06-14 20:30:49 +0200

    A few comments

--- src/main/java/com/github/rskupnik/storyteller/aggregates/TextEffects.java
+++ src/main/java/com/github/rskupnik/storyteller/aggregates/TextEffects.java
@@ -2,6 +2,9 @@ package com.github.rskupnik.storyteller.aggregates;
 
 import com.github.rskupnik.storyteller.effects.TextEffect;
 
+/**
+ * Aggregate class that holds all text effects registered in the engine.
+ */
 public final class TextEffects {
     public TextEffect clickEffect;
 }
