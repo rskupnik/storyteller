f6b545bcae4598fef8bc005df75b99c8feeb2b0b
Myzreal <r.skupnik@gmail.com>
2017-06-18 15:34:39 +0200

    Make the Typewriter process actors in proper order

--- src/main/java/com/github/rskupnik/storyteller/effects/appear/TypewriterAppearEffect.java
+++ src/main/java/com/github/rskupnik/storyteller/effects/appear/TypewriterAppearEffect.java
@@ -17,13 +17,16 @@ import org.javatuples.Quartet;
 import org.javatuples.Triplet;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class TypewriterAppearEffect extends AppearEffect {
 
     private List<Pair<Actor, ArrayList<Quartet<CharSequence, Rectangle, Vector2, Color>>>> data;
     //private TransformedScene data;
     private Typewriter typewriter;
+    private Map<Actor, List<Integer>> processingMap = new HashMap<>();
 
     public TypewriterAppearEffect() {
         typewriter = new Typewriter();
@@ -60,8 +63,27 @@ public final class TypewriterAppearEffect extends AppearEffect {
         if (font == null || batch == null)
             return; // Throw exception?
 
+        // Start typewriting the first element and set a flag
+        // If the flag is set, all further elements are ignored
+        // After typewriting is done, set another flag pointing that this actor is typewritten already
+        // A typewritten actor should simple be printed without a typewriter
+        // The next in line should start being typewritten and the flag needs to be set again
+        // Any further actors need to be ignored - until the one being typewritten is finished, the flag is reset, and so on...
+        int i = 0;
+        int currentlyProcessedActorIndex = -1;
         for (Pair<Actor, ArrayList<Quartet<CharSequence, Rectangle, Vector2, Color>>> actorToDataPair : data) {
             Actor actor = actorToDataPair.getValue0();
+            if (currentlyProcessedActorIndex == -1) {
+                currentlyProcessedActorIndex = i;
+            }
+
+            if (!actor.getInternalActor().isProcessed() && i != currentlyProcessedActorIndex) {
+                i++;
+                continue;
+            }
+            int currentlyProcessedFragmentIndex = -1;
+            int j = 0;
+            boolean allFragmentsProcessed = true;
             for (Quartet<CharSequence, Rectangle, Vector2, Color> actorData : actorToDataPair.getValue1()) {
                 // Unpack data
                 String str = (String) actorData.getValue0();
@@ -76,14 +98,64 @@ public final class TypewriterAppearEffect extends AppearEffect {
                 // Draw the GL
                 Color prevColor = font.getColor();
                 font.setColor(color != null ? color : prevColor);
-                font.draw(batch, typewriter.updateAndType(str, delta), position.x, position.y + actor.getInternalActor().getYOffset());
+
+                if (isProcessed(actor, j)) {
+                    font.draw(batch, str, position.x, position.y + actor.getInternalActor().getYOffset());
+                    j++;
+                    continue;
+                } else {
+                    allFragmentsProcessed = false;
+
+                    if (currentlyProcessedFragmentIndex == -1)
+                        currentlyProcessedFragmentIndex = j;
+
+                    if (currentlyProcessedFragmentIndex != j) {
+                        j++;
+                        continue;
+                    }
+
+                    CharSequence cs = typewriter.updateAndType(str, delta);
+                    font.draw(batch, cs, position.x, position.y + actor.getInternalActor().getYOffset());
+
+                    if (cs.length() == str.length()) {
+                        //actor.getInternalActor().setProcessed(true);
+                        List<Integer> actorsProcessedIndices = processingMap.get(actor);
+                        if (actorsProcessedIndices == null) {
+                            actorsProcessedIndices = new ArrayList<>();
+                            processingMap.put(actor, actorsProcessedIndices);
+                        }
+                        actorsProcessedIndices.add(currentlyProcessedFragmentIndex);
+
+                        currentlyProcessedFragmentIndex = -1;
+                        typewriter.getInterpolator().setTime(0);
+                    }
+                }
+
                 font.setColor(prevColor);
+
+                j++;
 
                 // Add the Rectangle to clickables
                 // TODO: Maybe this should be done somewhere else instead of render(), since it's a one-time action
                 //if (rectangle != null && scenePair.internal().isFirstDraw())
                 //    inputHandler.addClickable(scenePair.scene(), rectangle, actor);
             }
+
+            if (allFragmentsProcessed) {
+                actor.getInternalActor().setProcessed(true);
+                currentlyProcessedActorIndex = -1;
+            }
+
+            i++;
         }
+    }
+
+    private boolean isProcessed(Actor actor, int index) {
+        List<Integer> actorsProcessedIndices = processingMap.get(actor);
+        if (actorsProcessedIndices == null) {
+            actorsProcessedIndices = new ArrayList<>();
+            processingMap.put(actor, actorsProcessedIndices);
+        }
+        return actorsProcessedIndices.contains(index);
     }
 }
--- src/main/java/com/github/rskupnik/storyteller/peripheral/internals/InternalActor.java
+++ src/main/java/com/github/rskupnik/storyteller/peripheral/internals/InternalActor.java
@@ -3,6 +3,7 @@ package com.github.rskupnik.storyteller.peripheral.internals;
 public final class InternalActor {
 
     private float yOffset;
+    private boolean processed;  // Used in rendering and several AppearEffects to indicate processing status
 
     public float getYOffset() {
         return yOffset;
@@ -10,5 +11,13 @@ public final class InternalActor {
 
     public void setYOffset(float yOffset) {
         this.yOffset = yOffset;
+    }
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
     }
 }
