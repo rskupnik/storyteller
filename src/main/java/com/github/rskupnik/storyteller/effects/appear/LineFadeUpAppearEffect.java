83287e3af4fc6fa3cf4b8dcc9900b50132c14e4b
Myzreal <r.skupnik@gmail.com>
2017-06-19 14:53:53 +0200

    Create an empty LineFadeUp effect class

---
+++ src/main/java/com/github/rskupnik/storyteller/effects/appear/LineFadeUpAppearEffect.java
@@ -0,0 +1,25 @@
+package com.github.rskupnik.storyteller.effects.appear;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.github.rskupnik.storyteller.wrappers.complex.TransformedScene;
+
+public class LineFadeUpAppearEffect extends AppearEffect {
+
+    private Object data;
+
+    @Override
+    public Object getData() {
+        return data;
+    }
+
+    @Override
+    public void transform(TransformedScene input) {
+
+    }
+
+    @Override
+    public void render(float delta, SpriteBatch batch, BitmapFont font) {
+
+    }
+}
